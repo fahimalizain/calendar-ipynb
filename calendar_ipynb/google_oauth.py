7baecc9d07484aa447ec24160fbae53fe76bc3e6
Fahim Ali Zain <fahim.zain@kwalee.com>
2025-09-06 16:26:58 +0530

    feat: Event Renamer

--- calendar_ipynb/google_oauth.py
+++ calendar_ipynb/google_oauth.py
@@ -14,6 +14,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/calendar.readonly",
+    "https://www.googleapis.com/auth/calendar.events"
 ]
 
 
